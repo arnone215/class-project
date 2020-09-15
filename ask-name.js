6ce15097f51caaf23a32cf3cf5f003049ae94155
arnone215 <tarnone21589@gmail.com>
2020-09-14 21:34:40 -0700

    hello

--- ask-name.js
+++ ask-name.js
@@ -1,5 +1,13 @@
 'use strict'
 
-var userName = prompt('What is your name?');
+// var userName = prompt('What is your name?');
+// document.write('<h2>' + "Nice to meet you!" + userName + '</h2>');
 
-document.write('<h2>' + "Nice to meet you!" + userName + '</h2>');
+function askName() {
+    var userName = prompt('What is your name?');
+    document.write('<h2>' + "Nice to meet you!" + userName + '</h2>');
+}
+
+askName();
+// askName();
+// askName();
