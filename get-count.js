6eb87173b61555fa631e6371511564cb108ac602
arnone215 <tarnone21589@gmail.com>
2020-09-15 21:28:39 -0700

    hello

---
+++ get-count.js
@@ -0,0 +1,6 @@
+function getCount() {
+    var getCount = prompt('Great! How many times have you been?');
+    while(isNan(count) || count === '') {
+        count = prompt('Please enter a number. How many times have you been?');
+    }
+    return count; }
--- get-product.js
+++ get-product.js
@@ -1,14 +1,41 @@
 'use strict'
 
+// helper function
+function getProduct() {
 var response = prompt('Would you prefer hiking at Mt. Rainier or Mt. Hood?');
 var message;
 
-if (response === 'Mt. Rainier') {
-    message = 'How cliche!';
-} else if (response === 'Mt. Hood') {
-    message = 'A bit of a drive but okay!';
-} else {
-    message ='You don\'t get out much huh?';
+while(response !='Mt. Rainier' && response != 'Mt. Hood') {
+item = prompt('Would you rather hike at Mt. Rainier or Mt. Hood? SAY MT. RAINIER!');
 }
 
-document.write('<h3>' + message + '</h3>');
+if (response === 'Mt. Rainier') {
+    item = '<img src="images/bora-bora.jpg">';
+} else if (response === 'Mt. Hood') {
+    item = '<img src="images/mtshukasan.jpg">';
+} 
+
+return item;
+}
+// helper function
+function getCount() {
+    var getCount = prompt('Great! How many times have you been?');
+    while(isNan(count) || count === '') {
+        count = prompt('Please enter a number. How many times have you been?');
+    }
+    return count;
+}
+
+function showOrder() {
+    var result = '';
+    var itemType = getProduct();
+    var total = getCount();
+
+    for(var i = 0; i < total; i++) {
+        console.log(result);
+        result = result + '<p>' + itemType + '</p>';
+    }
+    return document.write(result);
+}
+
+showOrder();
